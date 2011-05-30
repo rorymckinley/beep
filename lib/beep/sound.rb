2ebd9e6216b90d1060083b1ea7f805af29816674
Rory McKinley <rorymckinley@gmail.com>
2011-05-30 23:01:45 +0200

    Messy method to add frequency parameter to beep - will get messier before it gets better

--- lib/beep/sound.rb
+++ lib/beep/sound.rb
@@ -1,7 +1,9 @@
 module Beep
   class Sound
-    def self.generate
-      system('beep')
+    def self.generate(instructions=[])
+      basic_command = 'beep'
+      basic_command += ' -f %s' % instructions[0][:frequency] if instructions.any? && instructions[0][:frequency]
+      system(basic_command)
     end
   end
 end
--- spec/sound_spec.rb
+++ spec/sound_spec.rb
@@ -1,8 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Beep::Sound do
-  it "should be able to call the default beep command" do
+  it "should be able to call the default beep command without arguments" do
     Beep::Sound.should_receive(:system).with('beep')
     Beep::Sound.generate
+  end
+  it "should allow a beep to be generated with a frequency" do
+    Beep::Sound.should_receive(:system).with('beep -f 100')
+    Beep::Sound.generate([{:frequency => 100}])
   end
 end
