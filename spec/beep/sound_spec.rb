9aac9baba86d775c60a9fbd16996d7e9d77e4605
Rory McKinley <rorymckinley@gmail.com>
2011-05-31 21:32:30 +0200

    Fixed spec_helper require after moving spec

--- spec/beep/sound_spec.rb
+++ spec/beep/sound_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Beep::Sound do
   it "should be able to call the default beep command without arguments" do
