3f9a71920fb0ef2d79ace4daa70fa4bc181882ee
Rory McKinley <rorymckinley@gmail.com>
2011-05-31 21:39:30 +0200

    Beep now caters for duration and pause

--- lib/beep/sound.rb
+++ lib/beep/sound.rb
@@ -3,6 +3,8 @@ module Beep
     def self.generate(instructions=[])
       basic_command = 'beep'
       basic_command += ' -f %s' % instructions[0][:frequency] if instructions.any? && instructions[0][:frequency]
+      basic_command += ' -l %s' % instructions[0][:duration] if instructions.any? && instructions[0][:duration]
+      basic_command += ' -D %s' % instructions[0][:pause] if instructions.any? && instructions[0][:pause]
       system(basic_command)
     end
   end
--- spec/beep/sound_spec.rb
+++ spec/beep/sound_spec.rb
@@ -5,8 +5,16 @@ describe Beep::Sound do
     Beep::Sound.should_receive(:system).with('beep')
     Beep::Sound.generate
   end
-  it "should allow a beep to be generated with a frequency" do
+  it "should allow a beep to be generated with a given frequency" do
     Beep::Sound.should_receive(:system).with('beep -f 100')
     Beep::Sound.generate([{:frequency => 100}])
+  end
+  it "should allow a beep to be generated with a given duration in milliseconds" do
+    Beep::Sound.should_receive(:system).with('beep -l 1000')
+    Beep::Sound.generate([{:duration => 1000}])
+  end
+  it "should allow a beep to be generated with a given pause after the beep" do
+    Beep::Sound.should_receive(:system).with('beep -D 1000')
+    Beep::Sound.generate([{:pause => 1000}])
   end
 end
