ccd645837ad72d3fcc51dd5c1b78e534f573e41a
Rory McKinley <rorymckinley@gmail.com>
2011-06-02 16:58:08 +0200

    First refactor - move the building of arguments out of generate method

--- lib/beep/sound.rb
+++ lib/beep/sound.rb
@@ -1,15 +1,20 @@
 module Beep
   class Sound
     def self.generate(instructions=[])
-      basic_command = 'beep'
-      basic_command += (instructions.map do |instruction|
+      basic_command = 'beep%s' % create_arguments(instructions)
+      system(basic_command)
+    end
+
+    private
+
+    def self.create_arguments(instructions)
+      (instructions.map do |instruction|
         instruction_as_args = ''
         instruction_as_args += ' -f %s' % instruction[:frequency] if instruction[:frequency]
         instruction_as_args += ' -l %s' % instruction[:duration] if instruction[:duration]
         instruction_as_args += ' -D %s' % instruction[:pause] if instruction[:pause]
         instruction_as_args
       end).join(" -n")
-      system(basic_command)
     end
   end
 end
